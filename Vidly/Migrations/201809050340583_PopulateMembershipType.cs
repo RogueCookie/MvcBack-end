a3127790e5f3dd410c6b7ce47d6ffc2cf5c9d6c3
Valeriia Vaganova <v9527906422@gmail.com>
2018-09-05 06:19:25 +0200

    Fill Sql into Up() method. we can modify database anyway we want, update-db

--- Vidly/Migrations/201809050340583_PopulateMembershipType.cs
+++ Vidly/Migrations/201809050340583_PopulateMembershipType.cs
@@ -7,6 +7,11 @@ namespace Vidly.Migrations
     {
         public override void Up()
         {
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (1, 0, 0, 0)");    //сиквел method and pass сиквел statement there
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (2, 30, 1, 10)");    //сиквел method and pass сиквел statement there
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (3, 90, 3, 15)");    //сиквел method and pass сиквел statement there
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (4, 300, 12, 20)");    //сиквел method and pass сиквел statement there
+                                                //columns                                    value(id=1).signup = 0 $, mjnth =0, discount=0)...
         }
         
         public override void Down()
