dfab5c351d1675cecbf433b1de434076944ccc98
Valeriia Vaganova <v9527906422@gmail.com>
2018-09-05 05:09:47 +0200

    Added migration AddIsSubscribedToCustomer

--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace Vidly.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        //public DbSet<Customer> Customers { get; set; } //represent customer's table
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
