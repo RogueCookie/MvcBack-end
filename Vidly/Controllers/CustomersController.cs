e965765706ab1412a258218baaf7deaf615fc1d3
Valeriia Vaganova <v9527906422@gmail.com>
2018-09-10 18:42:02 +0200

    All message errors output in the top of page

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -28,6 +28,8 @@ namespace Vidly.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),  //prop will be initialze by default value
+
                 MembershipTypes = membershipTypes
             };
 
