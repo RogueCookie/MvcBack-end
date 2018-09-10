ec69b4e4887e8e6ad4dbab1c336b44e0779c572e
Valeriia Vaganova <v9527906422@gmail.com>
2018-09-10 17:23:14 +0200

    before adding validation

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -16,6 +16,7 @@ namespace Vidly.Models
 
         public Genre Genre { get; set; }//navigation property
 
+        [Display(Name ="Genre")]
         public byte GenreId { get; set; } 
 
         public DateTime DateAdded { get; set; }
