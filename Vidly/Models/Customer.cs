9af7d4e5b0e935b9fcbd0105d9b647275220f14a
Valeriia Vaganova <v9527906422@gmail.com>
2018-09-10 18:07:15 +0200

    Added validation Min18Years

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,7 +10,7 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Please, enter customer's name.")]
         [StringLength(225)]
         public string Name { get; set; }
 
@@ -22,6 +22,7 @@ namespace Vidly.Models
         public byte MembershipTypeId { get; set; } //for optimization EF recognize that conventions and trets this prop as a foreign key
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; } //nullable
     }
 }   
