5a29db1cde7f0a72353ce94cf1e6911103a4675d
Valeriia Vaganova <v9527906422@gmail.com>
2018-09-22 23:08:11 +0200

    comments

--- Vidly/App_Start/MappingProfile.cs
+++ Vidly/App_Start/MappingProfile.cs
@@ -18,7 +18,8 @@ namespace Vidly.App_Start
 
             //Dto to Domain
             Mapper.CreateMap<CustomerDto, Customer>()
-                .ForMember(c => c.Id, opt => opt.Ignore());
+                .ForMember(c => c.Id, opt => opt.Ignore()); //prop Id id part of object's key information and cannot be modified
+            //the exeption is thrown when automapper attempts to set thr Id of customer/ and ignore Id duting mapping becausea  key ptop souldn't be changed
 
             Mapper.CreateMap<MovieDto, Movie>()
                 .ForMember(c => c.Id, opt => opt.Ignore());
