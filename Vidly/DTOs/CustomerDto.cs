555dade7945db4465ee114050c65e133ec5bca65
Valeriia Vaganova <v9527906422@gmail.com>
2018-09-22 03:10:43 +0200

    Added Data Transfer Object

---
+++ Vidly/DTOs/CustomerDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DTOs
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(225)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        //exclude because this is domail class and here that property is create dependency from Dto to domain model
+        //if we change this prop that impact Dto
+        //public MembershipType MembershipType { get; set; }  
+
+        public byte MembershipTypeId { get; set; }  
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; } 
+    }
+}
